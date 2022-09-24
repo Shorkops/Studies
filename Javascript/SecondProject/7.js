95b04a5abf5d9d05551164e6ece6edce973ac7bf
Annie <redpillseeker@gmail.com>
2022-09-24 18:24:48 +0200

    Problem giving problems

---
+++ Javascript/SecondProject/7.js
@@ -0,0 +1,49 @@
+// let walk = ['n','s','n','s','n','s','n','s','n','s'];
+
+// function isValidWalk(walk) {
+//     let n = 0;
+//     let e = 0;
+    
+//     for (let i = 0; i < walk.length; i++){
+//         switch(walk[i]){
+//             case 'n':
+//                 n++
+//                 console.log(i)
+//                 break;
+//             case 's':
+//                 n--
+//                 console.log(i)
+//                 break;
+//             case 'e':
+//                 e++
+//                 console.log(i)
+//                 break;
+//             case 'w':
+//                 e--
+//                 console.log(i)
+//                 break;
+//         }
+//         if (n == 0 && e == 0 && walk.length < 11){
+//             return true
+//         } else {
+//             return false
+//         }
+//     }
+// }
+
+// console.log(isValidWalk(walk));
+
+function persistence(num) {
+    let arr = arr.from(num);
+    if (num.length < 2){
+       return 0;
+    } else {
+        for (let i = 0; i < arr.length; i++){
+            arr[0];
+        }
+    }
+    // if length < 2, return 0, else
+    // separate and multiply individuals
+    // if > 9, repeat
+    // return number of times it had to be done, counter
+    }
--- Javascript/SecondProject/7.ts
+++
@@ -1,6 +0,0 @@
-
-const ex = draw => 1.+(1);
-
-//console.log(typeof ex());
-
-console.log(typeof (1.+(1)))
