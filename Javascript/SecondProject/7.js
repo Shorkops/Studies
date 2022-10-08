afcb8be9c95b23e1e39e5126843cb69ecee2b958
Annie <redpillseeker@gmail.com>
2022-10-08 18:28:40 +0200

    Now rustier!

--- Javascript/SecondProject/7.js
+++ Javascript/SecondProject/7.js
@@ -33,17 +33,37 @@
 
 // console.log(isValidWalk(walk));
 
-function persistence(num) {
-    let arr = arr.from(num);
-    if (num.length < 2){
-       return 0;
-    } else {
-        for (let i = 0; i < arr.length; i++){
-            arr[0];
+// function persistence(num) {
+//     let arr = arr.from(num);
+//     if (num.length < 2){
+//        return 0;
+//     } else {
+//         for (let i = 0; i < arr.length; i++){
+//             arr[0];
+//         }
+//     }
+//     // if length < 2, return 0, else
+//     // separate and multiply individuals
+//     // if > 9, repeat
+//     // return number of times it had to be done, counter
+//     }
+
+//arrayDiff
+
+const arra = [1,2,2,2,3];
+const arrb = [2];
+
+function arrayDiff(a, b) {
+let resultArray = [];
+  for (let i = 0; i < a.length; i++){
+    for (let j = 0; j < b.length; j++){
+        if (a[i] !== b[j]){
+            resultArray.push(a[i]);
         }
     }
-    // if length < 2, return 0, else
-    // separate and multiply individuals
-    // if > 9, repeat
-    // return number of times it had to be done, counter
-    }
+  }0
+}
+
+console.log(arrayDiff(arra, arrb));
+
+let java;
